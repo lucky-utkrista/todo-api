d38ecf86847e15c96a4eea531ee2688963e635f4
utkrista <kcutkrista92@gmail.com>
2018-09-16 15:52:47 +0545

    error stil heroku deployment

--- server/server.js
+++ server/server.js
@@ -16,16 +16,15 @@ var todo = new Todo({
 });
 Todo.save().then((doc)=>{
   res.send(doc);
-    }).catch((e)=>{
-//},(e)=>{
+
+},(e)=>{
   res.status(400).send(e);
 });
 });
 app.get('/todos',(req,res)=>{
   Todo.find().then((todos)=>{
   res.send({todos});
-  // },(e)=>{
-  }).catch((e)=>{
+  },(e)=>{
     res.status(400).send(e);
   });
 });
