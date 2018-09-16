9cd3ad742c8e61ae2a866a447c1df8228f9a880b
utkrista <kcutkrista92@gmail.com>
2018-09-16 15:27:03 +0545

    error finding for heroku

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-const {ObjectID} = require('mongodb');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -13,19 +13,19 @@ app.use(bodyParser.json());
 app.post('/todos',(req,res)=>{
 var todo = new Todo({
   text: req.body.text
-})
+});
 todo.save().then((doc)=>{
   res.send(doc);
 },(e)=>{
   res.status(400).send(e);
-})
+});
 });
 app.get('/todos',(req,res)=>{
   Todo.find().then((todos)=>{
   res.send({todos});
   },(e)=>{
     res.status(400).send(e);
-  })
+  });
 });
 //GET /todos/1231232
 app.get('/todos/:id',(req,res)=>{
@@ -44,7 +44,6 @@ Todo.findById(id).then((todo)=>{
 }).catch((e)=>{
   res.status(400).send();
 });
-
 
 });
 
