4ac1570224c3b23067738ed5367da33165d6f70f
utkrista <kcutkrista92@gmail.com>
2018-09-16 16:25:25 +0545

    error stil heroku deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp')
 .then(() => {
 console.log("Connected to Database");
 }).catch((err) => {
