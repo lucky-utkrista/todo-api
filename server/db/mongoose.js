bd67b8151e64c35136f29d45fa79682df6f5f5f8
utkrista <kcutkrista92@gmail.com>
2018-09-17 12:44:53 +0545

    try error

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,13 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
-
+mongoose.connection.on("connected", () => {
+    console.log("Connected to database");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("Database error:" + err);
+});
 
 module.exports ={mongoose};
