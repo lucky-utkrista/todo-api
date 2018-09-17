5f1c37716ff5c12e3a7299df41f0019bb9e11c6f
utkrista <kcutkrista92@gmail.com>
2018-09-17 13:53:37 +0545

    in process:

--- server/server.js
+++ server/server.js
@@ -20,32 +20,32 @@ todo.save().then((doc)=>{
   res.status(400).send(e);
 });
 });
-// app.get('/todos',(req,res)=>{
-//   Todo.find().then((todos)=>{
-//   res.send({todos});
-//   },(e)=>{
-//     res.status(400).send(e);
-//   });
-// });
-// //GET /todos/1231232
-// app.get('/todos/:id',(req,res)=>{
-//   var id = req.params.id;
-//   //valid id using isValid
-//   if(!ObjectID.isValid(id)){
-//     return res.status(404).send();
-//  }
-//
-// //findById
-// Todo.findById(id).then((todo)=>{
-//   if(!todo){
-//   return res.status(404).send();
-//   }
-//   res.send({todo});
-// }).catch((e)=>{
-//   res.status(400).send();
-// });
-//
-// });
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+  res.send({todos});
+  },(e)=>{
+    res.status(400).send(e);
+  });
+});
+//GET /todos/1231232
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  //valid id using isValid
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+ }
+
+//findById
+Todo.findById(id).then((todo)=>{
+  if(!todo){
+  return res.status(404).send();
+  }
+  res.send({todo});
+}).catch((e)=>{
+  res.status(400).send();
+});
+
+});
 
 app.listen(port,()=>{
   console.log(`started up at port ${port}`);
