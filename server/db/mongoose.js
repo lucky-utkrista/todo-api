3ba1e42684e566f64a45c086e5a9e8a6df1b41e0
utkrista <kcutkrista92@gmail.com>
2018-09-17 13:19:07 +0545

    heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,13 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
 
-mongoose.connection.on("connected", () => {
-    console.log("Connected to database");
-});
-mongoose.connection.on("error", (err) => {
-  console.log("Database error:" + err);
-});
 
 module.exports ={mongoose};
