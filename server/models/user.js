d15055faddaf9e83bda4ae6e9cc21539dc6de384
utkrista <kcutkrista92@gmail.com>
2018-09-19 07:19:04 +0545

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -47,8 +47,15 @@ UserSchema.methods.generateAuthToken = function(){
   return user.save().then(()=>{
     return token;
   });
+};
 
-
+UserSchema.methods.removeToken = function(token){
+  var user = this;
+  return user.update({
+    $pull:{
+      tokens:{token}
+    }
+  });
 };
 
 UserSchema.statics.findByToken = function (token){
